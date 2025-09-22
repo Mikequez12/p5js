4099290074935607d88b60307dc7cbe5a0a5ccde
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-09-22 12:02:57 +0200

    Update sketch.js

--- clock/sketch.js
+++ clock/sketch.js
@@ -3,6 +3,7 @@ function setup() {
 }
 
 function draw() {
+  textFont('serif')
   clear()
   let now = new Date()
   let s = now.getSeconds()+now.getMilliseconds()/1000
@@ -15,8 +16,8 @@ function draw() {
   line(
     width/2,
     height/2,
-    cos(radians(s/60*360-90))*width/5*1.6+width/2,
-    sin(radians(s/60*360-90))*height/5*1.6+height/2
+    cos(radians(s/60*360-90))*width/5*1.4+width/2,
+    sin(radians(s/60*360-90))*height/5*1.4+height/2
   )
   drawingContext.shadowBlur = 10;
   drawingContext.shadowColor = color('white');
@@ -25,26 +26,25 @@ function draw() {
   line(
     width/2,
     height/2,
-    cos(radians(m/60*360-90))*width/2.9+width/2,
-    sin(radians(m/60*360-90))*height/2.9+height/2
+    cos(radians(m/60*360-90))*width/3.2+width/2,
+    sin(radians(m/60*360-90))*height/3.2+height/2
   )
   strokeWeight(3)
   line(
     width/2,
     height/2,
-    cos(radians(h/12*360-90))*width/4+width/2,
-    sin(radians(h/12*360-90))*height/4+height/2
+    cos(radians(h/12*360-90))*width/4.2+width/2,
+    sin(radians(h/12*360-90))*height/4.2+height/2
   )
   drawingContext.shadowBlur = 0;
   textSize(20)
   textAlign(CENTER,CENTER)
   for (i=0;i<12;i++) {
+    noStroke()
     if (i==floor((h%12)-1)) {
-      stroke(color('white'))
-      fill(color('white'))
+      fill(color('#f00f'))
     } else {
-      stroke(color('gray'))
-      fill(color('gray'))
+      fill(color('#ffff'))
     }
     text(
       ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII'][i],
@@ -68,6 +68,33 @@ function draw() {
         i+1,
         (width/11*(4.3))*sin(radians(i/12/5*360))+width/2,
         (height/11*(-4.3))*cos(radians(i/12/5*360))+height/2
+      )
+    }
+    if (i%5==0) {
+      continue
+    }
+    stroke(color('gray'));
+    point(
+      width/11*(-5)*sin(radians(i/12/5*360))+width/2,
+      height/11*5*cos(radians(i/12/5*360))+height/2
+    )
+  }
+  for (i=0;i<5*12;i++) {
+    noStroke()
+    drawingContext.shadowBlur = 0;
+    fill(color('#fffa'))
+    d = max(0,3-abs(i-s))**2*2
+    if (i==floor(s)) {
+      fill(color('#f00a'))
+    }
+    let circularDist = min(abs(i - s), 60 - abs(i - s))
+    let sizeFactor = (sqrt(max(0, 12 - circularDist))/1)**2/1.2
+    textSize(sizeFactor)
+    if (sizeFactor>0) {
+      text(
+        i+1,
+        (width/11*(3.8))*sin(radians(i/12/5*360))+width/2,
+        (height/11*(-3.8))*cos(radians(i/12/5*360))+height/2
       )
     }
     if (i%5==0) {
