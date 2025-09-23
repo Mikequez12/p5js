8c0cccf29139046d19b7832114009ab73683ae7c
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-09-23 16:47:50 +0200

    Update sketch.js

--- clock/sketch.js
+++ clock/sketch.js
@@ -6,24 +6,27 @@ function draw() {
   noFill()
   strokeWeight(1)
   stroke(color('white'))
-  fill(color('white'))
+  noFill()
   textFont('serif')
-  background(color('black'))
-  drawingContext.shadowBlur = 50;
+  clear()
+  drawingContext.shadowBlur = 10;
   drawingContext.shadowColor = color('white');
-  for (let i = 0; i < 360/5; i++) {
-    line(
-      sin(i/5)*width/2.2+width/2,
-      cos(i/5)*width/2.2+height/2,
-      sin((i+1)/5)*width/2.2+width/2,
-      cos((i+1)/5)*width/2.2+height/2,
-    )
+  for (let i = 0; i < 12; i++) {
+    beginShape();
+    for (let j = 0; j < 10; j++) {
+      if (j<2||j>8) continue;
+      vertex(
+        sin(radians(i/12*360+j/10*(360/12)))*width/2.2+width/2,
+        cos(radians(i/12*360+j/10*(360/12)))*width/2.2+height/2
+      )
+    }
+    endShape()
   }
   let now = new Date()
   let s = now.getSeconds()+now.getMilliseconds()/1000
   let m = now.getMinutes()+s/60
   let h = now.getHours()+m/60
-  drawingContext.shadowBlur = 50;
+  drawingContext.shadowBlur = 5;
   drawingContext.shadowColor = color('red');
   stroke(color('red'))
   strokeWeight(1)
@@ -56,13 +59,11 @@ function draw() {
   for (i=0;i<12;i++) {
     noStroke();
     fill(color('black'))
-    circle(
-      width/11*(-5)*sin(radians(180+(i-11)/12*360))+width/2,
-      height/11*5*cos(radians(180+(i-11)/12*360))+height/2,
-      width/10
-    )
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = color('white');
     if (i==floor((h-1)%12)) {
       fill(color('red'))
+      drawingContext.shadowColor = color('red');
     } else {
       fill(color('white'))
     }
@@ -77,11 +78,14 @@ function draw() {
     drawingContext.shadowBlur = 0;
     d = max(0,3-abs(i-m))**2*2;
     let circularDist = min(abs(i - m), 60 - abs(i - m))
-    fill(lerpColor(color('white'),color('black'),(circularDist/10)))
+    let sizeFactor = (sqrt(max(0, 12 - circularDist))/1)**2*0.8
+    fill(lerpColor(color('black'),color('white'),(1-(circularDist/10))**2))
+    drawingContext.shadowBlur = 1;
+    drawingContext.shadowColor = color('white');
     if (i==floor(m)) {
       fill(color('red'))
+      drawingContext.shadowColor = color('red');
     }
-    let sizeFactor = (sqrt(max(0, 12 - circularDist))/1)**2*0.8
     textSize(sizeFactor)
     if (sizeFactor>0) {
       text(
@@ -95,12 +99,15 @@ function draw() {
     noStroke()
     drawingContext.shadowBlur = 0;
     let circularDist = min(abs(i - s), 60 - abs(i - s))
-    fill(lerpColor(color('white'),color('black'),(circularDist/10)))
+    let sizeFactor = (sqrt(max(0, 12 - circularDist))/1)**2*0.8
+    fill(lerpColor(color('black'),color('white'),(1-(circularDist/10))**2))
     d = max(0,3-abs(i-s))**2*2
+    drawingContext.shadowBlur = 1;
+    drawingContext.shadowColor = color('white');
     if (i==round(s)) {
-      fill(color('#f00'))
+      fill(color('#f00'));
+      drawingContext.shadowColor = color('red');
     }
-    let sizeFactor = (sqrt(max(0, 12 - circularDist))/1)**2
     textSize(sizeFactor)
     if (sizeFactor>0) {
       text(
